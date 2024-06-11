3ccc4a689400665cc6222b1ddf330f5e113f130f
Rohit <145928673+rohit0o07@users.noreply.github.com>
2024-06-11 15:14:22 +0530

    hygraph updated

--- app/(router)/courses/_components/CourseItem.jsx
+++ app/(router)/courses/_components/CourseItem.jsx
@@ -4,27 +4,34 @@ import React from 'react'
 function CourseItem({course}) {
   return (
     <div className='border rounded-xl hover:shadow-md hover:shadow-purple-300 cursor-pointer'>
-      <Image src={course?.banner?.url} width={500} height={150} alt='banner'
+        <Image  src={course?.banner?.url} 
+        width={500} 
+        height={150} 
+        alt='banner' 
         className='rounded-t-xl' />
-          <div className='flex flex-col gap-1 p-2'>
-        <h2 className='font-medium'>{course.name}</h2>
-        <h2 className='text-[10px] text-gray-400' >{course.author}</h2>
-
-        {course?.chapter?.length ==0 ?
+        <div className='flex flex-col gap-1'>
+          <h2 className='font-medium'>{course.name}</h2>
+          <h2 className='text-[12px] text-gray-400'>{course.author}</h2>
+        {course?.chapter?.length ==0 ?  
           <div className='flex gap-2'>
-          <Image src='/youtube.png' alt='youtube' width={20} height={20} />
-          <h2 className='text-[14px] text-gray-400'>Watch on Youtube</h2>
-        </div>:
-        <div className='flex gap-2'>
-          <Image src='/chapter.png' alt='chapter' width={20} height={20} />
-          <h2 className='text-[14px] text-gray-400'>Chapters</h2>
+            <Image src='/youtube.png'
+              alt='youtube'
+              width={20}
+              height={20}
+              />
+              <h2 className='text-[14px] text-gray-400'>Watch on Youtube</h2>
+          </div>:
+          <div className='flex gap-2'>
+            <Image src='/chapter.png'
+              alt='chapter'
+              width={20}
+              height={20}
+              />
+              <h2 className='text-[14px] text-gray-400'>Chapters</h2>
+          </div>}
+          <h2 className='text-[15px]'>{course?.free?'Free':'Paid'}</h2>
         </div>
-        }
-        <h2 className='text-[15px]'>{course?.free?'Free':'Paid'}</h2>
-        </div>
-        </div>
-
-    
+    </div>
   )
 }
 
--- app/-utils/GlobalApi.js
+++ app/-utils/GlobalApi.js
@@ -4,31 +4,32 @@ const MASTER_URL = `https://api-ap-south-1.hygraph.com/v2/${process.env.NEXT_PUB
 
 const getAllCourseList = async () => {
   const query = gql`
-    query MyQuery {
-      couseLists(first: 10, orderBy: createdAt_DESC) {
-        author
-        name
+   query MyQuery {
+  courseLists(first: 10, orderBy: createdAt_DESC) {
+    author
+    name
+    id
+    free
+    description
+    demoUrl
+    banner {
+      url
+    }
+    chapter {
+      ... on Chapter {
         id
-        free
-        description
-        demoUrl
-        banner {
+        name
+        video {
           url
         }
-        chapter {
-          ... on Chapter {
-            id
-            name
-            video {
-              url
-            }
-          }
-        }
-        totalChapters
-        sourceCode
-        tags
       }
     }
+    totalChapters
+    sourceCode
+    tags
+  }
+}
+
   `;
 
   try {
