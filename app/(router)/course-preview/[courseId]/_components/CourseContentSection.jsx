157be3fbd62a82463ae2c2b9bb57b8b17f51e381
Rohit <145928673+rohit0o07@users.noreply.github.com>
2024-06-12 20:53:51 +0530

    updated clerk authentication

--- app/(router)/course-preview/[courseId]/_components/CourseContentSection.jsx
+++ app/(router)/course-preview/[courseId]/_components/CourseContentSection.jsx
@@ -1,16 +1,21 @@
-import { Lock } from 'lucide-react'
-import React from 'react'
+import { Lock, Play } from 'lucide-react'
+import React, { useState } from 'react'
 
 function CourseContentSection({courseInfo}) {
+  const [activeIndex,setActiveIndex] = useState(0);
   return (
-    <div className='p-3 bg-white rounded-sm'>
+    <div className='p-3 bg-white rounded-sm mt-3'>
       <h2>Contents</h2>
       {
         courseInfo.chapter.map((item,index) => (
         <div>
-            <h2 className='p-2 text-[14px] flex justify-between m-2 hover:bg-gray-200 items-center border rounded-sm px-4 cursor-pointer'>
-            {index}.{item.name}
-            <Lock className='h-4 w-4'/></h2>
+            <h2 className={`p-2 text-[14px] flex justify-between m-2 hover:bg-gray-200 hover:text-gray-500
+              items-center border rounded-sm px-4 cursor-pointer${activeIndex == index &&'bg-primary text-white'}`}>
+            {index+1}.{item.name}
+            {activeIndex == index?
+              <Play className='h-4 w-4'/>:
+            
+            <Lock className='h-4 w-4'/>}</h2>
         </div>
       ))}
     </div>
